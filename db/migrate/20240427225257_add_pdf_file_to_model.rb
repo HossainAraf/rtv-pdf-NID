0f9617c2eb77719a3f40d5cbf07c8a6ee4dc36f5
Hossain <arafat.kd99@gmail.com>
2024-04-28 00:39:19 +0100

    Creatre database

--- db/migrate/20240427225257_add_pdf_file_to_model.rb
+++
@@ -1,5 +0,0 @@
-class AddPdfFileToModel < ActiveRecord::Migration[7.1]
-  def change
-    add_column :models, :pdf_file, :string
-  end
-end
---
+++ db/migrate/20240427233402_add_pdf_to_models.rb
@@ -0,0 +1,4 @@
+class AddPdfToModels < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
